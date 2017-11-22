1aabab6297266cf5b381fe32c3b342889599379c
ethanbeez <ethanbeez@gmail.com>
2017-11-21 17:48:25 -0800

    We made open and claw commands. I think.

--- src/org/usfirst/frc/team997/robot/commands/AutonomousClawThing.java
+++ src/org/usfirst/frc/team997/robot/commands/AutonomousClawThing.java
@@ -3,40 +3,22 @@ package org.usfirst.frc.team997.robot.commands;
 import org.usfirst.frc.team997.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  *
  */
-public class AutonomousClawThing extends Command {
+public class AutonomousClawThing extends CommandGroup {
+	
 
     public AutonomousClawThing() {
-        requires(Robot.driveTrain);
+    	addSequential (new OpenClaw());
+        addSequential (new DriveTo(30));
+        addSequential (new CloseClaw());
+        //Lift claw plz :^)
+      
+        
+        
         //requires(Robot.claw); //uncomment these
-    }
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	//Robot.claw.openClaw();
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	//drive to the bucket
-    	//Robot.claw.closeClaw();
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
     }
 }
