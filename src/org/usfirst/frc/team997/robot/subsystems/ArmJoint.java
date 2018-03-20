74dde5ee79dd00154bed1690f3e36a515495e028
Floyd Moore <floyd.moore@hp.com>
2018-03-20 14:59:58 -0700

    Some Fixes for moving the arm

--- src/org/usfirst/frc/team997/robot/subsystems/ArmJoint.java
+++ src/org/usfirst/frc/team997/robot/subsystems/ArmJoint.java
@@ -88,7 +88,6 @@ public class ArmJoint extends Subsystem {
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
-		setDefaultCommand(new LockArm());
 	}
 
 	public void stop() {
